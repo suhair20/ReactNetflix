aae6138e2f712253f04d6f84a022962825e2b265
suhair20 <moidheensuhair@gmail.com>
2024-06-05 22:30:02 -0700

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='uppercase'>
+      <h1 className='font-nsans-light'>netflix</h1>
+      <h1 className='font-nsans-medium'>netflix</h1>
+      <h1>netflix</h1>
+      <h1 className='font-nsans-bold'>netflix</h1>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
